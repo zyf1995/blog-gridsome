e6910b6f2ec10bf98d41cb694d48d80b246cff7c
zyf <970695936@qq.com>
2020-12-18 16:40:14 +0800

    'test'

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,20 +7,20 @@
 module.exports = {
   siteName: 'Gridsome',
   plugins: [
-    {
-      use: '@gridsome/source-strapi',
-      options: {
-        apiURL: 'http://localhost:1337',
-        queryLimit: 1000, // Defaults to 100
-        contentTypes: ['post'],
-        // singleTypes: ['impressum'],
-        // Possibility to login with a Strapi user,
-        // when content types are not publicly available (optional).
-        loginData: {
-          identifier: '',
-          password: ''
-        }
-      }
-    }
+    // {
+    //   use: '@gridsome/source-strapi',
+    //   options: {
+    //     apiURL: 'http://localhost:1337',
+    //     queryLimit: 1000, // Defaults to 100
+    //     contentTypes: ['post'],
+    //     // singleTypes: ['impressum'],
+    //     // Possibility to login with a Strapi user,
+    //     // when content types are not publicly available (optional).
+    //     loginData: {
+    //       identifier: '',
+    //       password: ''
+    //     }
+    //   }
+    // }
   ]
 }
